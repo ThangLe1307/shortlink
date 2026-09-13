56dd9295ae2add3bf7efc2349416cfd7b73e045c
thienthang.letran <letranthienthang@gmail.com>
2026-09-13 21:45:27 +0700

    feat: add local and redis caching

--- link-api/src/main/java/com/shortlink/linkapi/service/impl/ApiKeyServiceImpl.java
+++ link-api/src/main/java/com/shortlink/linkapi/service/impl/ApiKeyServiceImpl.java
@@ -1,12 +1,14 @@
 package com.shortlink.linkapi.service.impl;
 
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
 import com.shortlink.linkapi.repository.UserRepository;
 import com.shortlink.linkapi.security.AuthenticatedUser;
 import com.shortlink.linkapi.service.ApiKeyService;
 import lombok.RequiredArgsConstructor;
-import org.apache.catalina.User;
 import org.springframework.stereotype.Service;
 
+import java.time.Duration;
 import java.util.Optional;
 
 @Service
@@ -15,13 +17,18 @@ public class ApiKeyServiceImpl implements ApiKeyService {
 
     private final UserRepository userRepository;
 
+    // local user cache
+    private final Cache<String, Optional<AuthenticatedUser>> userCache = Caffeine.newBuilder()
+            .expireAfterWrite(Duration.ofSeconds(60))
+            .maximumSize(10_000)
+            .build();
+
     @Override
     public Optional<AuthenticatedUser> getAuthenticatedUser(String apiKey) {
 
-        return userRepository.findByApiKeyHash(apiKey)
-                .map(
-                        userEntity ->
-                                new AuthenticatedUser(userEntity.getId(), userEntity.getEmail())
-                );
+        return userCache.get(apiKey, s
+                -> userRepository.findByApiKeyHash(s).map(userEntity
+                -> new AuthenticatedUser(userEntity.getId(), userEntity.getEmail()))
+        );
     }
 }
